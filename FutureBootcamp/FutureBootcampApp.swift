fe4b318a597e93e532bbbc8e54310ed2e64df161
superkookai <weerawut@live.com>
2025-03-15 14:22:40 +0700

    Initial Commit

---
+++ FutureBootcamp/FutureBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FutureBootcampApp.swift
+//  FutureBootcamp
+//
+//  Created by Weerawut Chaiyasomboon on 15/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct FutureBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
